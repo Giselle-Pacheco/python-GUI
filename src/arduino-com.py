fe9951c794a4a8236bcc69b2a7d36ee5a2b6ce02
GiselleP <pachecog2706@gmail.com>
2023-11-23 21:16:34 -0600

    added yt files

--- src/arduino-com.py
+++ src/arduino-com.py
@@ -5,3 +5,6 @@ datos=ard.readline()
 while 1:
     print(datos)
     time.sleep(3)
+
+#    https://www.youtube.com/watch?v=nZF9SwhmPRo&list=PLU8oAlHdN5BlvPxziopYZRd55pdqFwkeS&index=46 
+# https://www.youtube.com/watch?v=fCuFDW1RoxI

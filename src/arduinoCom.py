90a90d1cef66b74ef05a952960a630f0a917119d
unknown <pachecog2706@gmail.com>
2023-11-26 03:04:33 -0600

    All buttons added

--- src/arduinoCom.py
+++ src/arduinoCom.py
@@ -1,11 +1,27 @@
 import serial, time
+from tkinter import *
 
-def communicationArduino():
-    ard = serial.Serial('COM12',9600)
-    datos=ard.readline()
-    while 1:
-        print(datos.decode('utf8'))
-        time.sleep(3)
+class SerialCommunication():
+
+    def __init__(board, port, baudrate): #Here we use board to refer too the instance of the specific board in use
+        board.port = port
+        board.baudrate = baudrate
+        board.data = []
+        board.connection = None
+        board.connected = False
+        board.gettingData = False #isReading
+        board.samplesCount = 0
+
+        try:
+            board.connection = serial.Serial(port, baudrate)
+            board.connected = True
+        except:
+            print(f"Error connecting to Arduino:")
+            board.connected = False
+
+            
+
+
 
 #    https://www.youtube.com/watch?v=nZF9SwhmPRo&list=PLU8oAlHdN5BlvPxziopYZRd55pdqFwkeS&index=46 
 # https://www.youtube.com/watch?v=fCuFDW1RoxI
--- src/main.py
+++ src/main.py
@@ -1,35 +1,96 @@
-from tkinter import *
+
+import tkinter as tk
+#ttk isstlye the widgets
+from tkinter import ttk
 import serial, time
-from arduinoCom import communicationArduino
+#Conversion para arhivo csv
+import csv
+import matplotlib.pyplot as plt
+# Librería para la grafica en tiempo real
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg 
+#un hilo hace a la par un programa
+import threading
+from arduinoCom import SerialCommunication
+global led
 
-#Configuring window layer
-window_layer = Tk()
-window_layer.title("ARDUINO - Python GUI")
-window_layer.resizable(1,1)
-# window_layer.iconbitmap(r"home/giselle/Documents/UDEM/Ot23/python-GUI/src/arduino_22429.ico")
+class GUIDesign:
+    
+    def __init__(board, root):
+        board.windowLayer = root
+       
+        # se crea una instancia de la clase ArduinoComunication
+        board.windowLayer.title("ARDUINO - Python GUI")
+        # board.windowLayer.resizable(1,1)
 
-#Configuring frame to setup components
-frame=Frame()
-frame.pack()
-frame.config(width="1500", height="900")
-frame.config(bd=10)
-frame.config(relief="sunken")
+        #Configuring frame to setup components
+        # Frame para controles y botones
+        board.frame = ttk.Frame(root)
+        board.frame.pack(side=tk.LEFT, fill=tk.X)
 
-#button cursor config
-connect=Button(frame,text="Connect",command=communicationArduino)
-connect.place(x=180,y=200)
-# xxx.config(cursor="hand2")
+        # board.arduino = SerialCommunication(port='COM3', baudrate=9600)
 
-#Intro
-# cuadroTexto=Entry(frame)
-# cuadroTexto.place(x=100,y=200)
+    
+        board.StatusLabel = tk.Label(board.frame, text="Status:")
+        board.StatusLabel.grid(row=0,column=0, sticky="w")
 
-#Setting labels
-Label(frame, text= "COM PORT").place(x=100,y=200)
+        board.ledConnected = tk.Canvas(board.frame, width=30, height=30, bg='#ECECEC')
+        board.ledConnectedStatus = board.ledConnected.create_oval(3, 3, 15, 15, fill='red')
+        board.ledConnected.grid(row=0, column=1, sticky="n")
+
+        board.connectButton = ttk.Button(board.frame, text="Connect Port")#, command=board.toggleConnection)
+        board.connectButton.grid(row=0, column=2, sticky="n")
+
+        # Segunda columna - Prueba 1
+        board.labelSamples = ttk.Label(board.frame, text="Number of Samples:")
+        board.labelSamples.grid(row=1, column=0, pady=5,sticky="w")    
+        
+        board.entrySamples = ttk.Entry(board.frame)
+        board.entrySamples.grid(row=1, column=1, pady=5,sticky="w")
+
+        board.labelTime = ttk.Label(board.frame, text="Time:")
+        board.labelTime.grid(row=2, column=0,pady=5, sticky="w")    
+        
+        board.entryTime = ttk.Entry(board.frame)
+        board.entryTime.grid(row=2, column=1, pady=5,sticky="w")
+        
+        board.labelSeconds = ttk.Label(board.frame, text="s")
+        board.labelSeconds.grid(row=2, column=2,pady=5, sticky="w") 
+
+        board.labelFilename = ttk.Label(board.frame, text="CSV Filename:")
+        board.labelFilename.grid(row=3, column=0, pady=5, sticky="w")    
+        
+        board.entryFilename = ttk.Entry(board.frame)
+        board.entryFilename.grid(row=3, column=1, pady=5, sticky="w")
+
+        board.SaveButton = ttk.Button(board.frame, text="Save")#, command=board.toggleConnection)
+        board.SaveButton.grid(row=3, column=2, pady=5,sticky="n")
+
+        board.labelSamplingTime = ttk.Label(board.frame, text="Sampling Time:")
+        board.labelSamplingTime.grid(row=4, column=0, pady=5, sticky="w")    
+        
+        board.entrySamplingTime = ttk.Entry(board.frame)
+        board.entrySamplingTime.grid(row=4, column=1, pady=5, sticky="w")
+        
+        board.labelSecondsSampling = ttk.Label(board.frame, text="s")
+        board.labelSecondsSampling.grid(row=4, column=2,pady=5, sticky="w") 
+
+        board.FirstTestutton = ttk.Button(board.frame, text="Test 1")#, command=board.toggleConnection)
+        board.SaveButton.grid(row=5, column=1,pady=10, sticky="n")
+
+        board.SecondTestButton = ttk.Button(board.frame, text="Test 2")#, command=board.toggleConnection)
+        board.SaveButton.grid(row=5, column=2, pady=10,sticky="n")
+
+        board.TestStarted = tk.Canvas(board.frame, width=35, height=35, bg='#ECECEC')
+        board.TestStartedStatus = board.TestStarted.create_oval(15, 15, 30, 30, fill='red')
+        board.TestStarted.grid(row=5, column=0, pady=5, sticky="n")
+
+        board.testStatuslabel = ttk.Label(board.frame, text="Test ON")
+        board.testStatuslabel.grid(row=6, column=0, sticky="n")  
+
+       
+
+windowLayer =tk.Tk() #Configuring window layer
+ide=GUIDesign(windowLayer)
+windowLayer.mainloop() 
 
 
-
-
-
-window_layer.mainloop()
- 
